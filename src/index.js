e4d9529ec145980c16f793b6800d53f5901826b8
DummyClock <eliassaied23@gmail.com>
2025-02-09 02:27:34 -0500

    Completed API endpoint implementations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 9999;
+const { v4: uuidv4 } = require('uuid');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -9,9 +10,76 @@ app.use(express.json());
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
+// User info storage
+let users = [];                  
+
+// Print hello world
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// Add a new user
+app.post("/users", (req, res) => {
+    const { name, email} =  req.body;    // grab details from json msg
+
+    if(!name || !email){
+        return res.status(400).json({ message: "Missing name and/or email"});
+    }
+
+    newUser = {
+        id: uuidv4(),
+        name,
+        email,
+    };
+
+    users.push(newUser)
+    return res.status(201).json(newUser);
+});
+
+// Get user details using id
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id;       // grab id from uri
+    const foundUser = users.find((u => u.id == userId))
+
+    if(!foundUser){
+        return res.status(404).json({ message: "Provided id does not exist"})
+    }
+    return res.status(200).json(foundUser)
+}
+);
+
+// Update user details providing id
+app.put("/users/:id", (req, res) => {
+    const { name, email } = req.body;       // grab details from json msg
+    const userId = req.params.id;           // grab id from uri
+
+    foundUser = users.find((u => u.id == userId))
+    if (!name || !email){
+        return res.status(400).json( {message: "Missing name and/or email"})
+    }
+    if (!foundUser){
+        return res.status(404).json({message: "Provided id does not exist"})
+    }
+
+    //Update array
+    foundUser.name = name;
+    foundUser.email = email;
+    return res.status(200).json(foundUser)
+})
+
+// Delete user details providing id
+app.delete("/users/:id", (req, res) => {
+    const userId = req.params.id;       // grab id from uri
+
+    const foundUser = users.find((u => u.id == userId))
+    if(!foundUser)
+    {
+        return res.status(404).json({message: "No user with provided id"})
+    }
+    console.log("Preparing to delete user")
+    users.splice(users.indexOf(foundUser))
+    return res.status(204).json({message:"Successfully deleted user"})
+})
 
 // Do not touch the code below this comment
 // **************************************************************
